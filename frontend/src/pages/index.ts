3513a400ee9d9a0d4b3a927bbe3a75d7e688c6bd
DarwinMelaya <melayadarwin@gmail.com>
2026-07-14 22:18:28 +0800

    Add Login route and export in index.ts

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,2 +1,5 @@
 // landing page
 export { default as LandingPage } from "./landing-page/LandingPage";
+
+// auth
+export { default as Login } from "./auth/Login";
--- frontend/src/routers/Routers.tsx
+++ frontend/src/routers/Routers.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LandingPage } from "../pages";
+import { LandingPage, Login } from "../pages";
 
 const Routers = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
